bd751667e3a4c3b3f96c8b7f60a2b5e3bda4f9f8
mo49 <k.mochizuki.0409@gmail.com>
2017-02-19 23:52:30 +0900

    target scope

---
+++ mission01/Shooting_Game/Assets/Scripts/DestroyTimeout.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 自動で消えるスタート画面
+public class DestroyTimeout : MonoBehaviour
+{
+    public float timer = 15.0f;
+    void Start()
+    {
+        Destroy(gameObject, timer);
+    }
+}
