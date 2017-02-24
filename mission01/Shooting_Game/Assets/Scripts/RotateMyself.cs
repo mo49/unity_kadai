830a0481e1ac43c081e6fe89a48edc0585a3a7ff
mo49 <k.mochizuki.0409@gmail.com>
2017-02-25 01:15:57 +0900

    switch camera

---
+++ mission01/Shooting_Game/Assets/Scripts/RotateMyself.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMyself : MonoBehaviour {
+
+    public float speed = 0.05f;
+    public float timer = 10.0f;
+    private float count;
+    bool isOP = true;
+
+    // Use this for initialization
+    void Start () {
+        Invoke("Stop", timer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isOP) return;
+        transform.Rotate(0, speed * count, 0, Space.World);
+        count += Time.deltaTime;
+    }
+
+    void Stop()
+    {
+        isOP = false;
+    }
+}
---
+++ mission01/Shooting_Game/Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour {
+
+    private GameObject MainCam;
+    private GameObject SubCam;
+
+    IEnumerator Start()
+    {
+        MainCam = GameObject.Find("Main Camera");
+        SubCam = GameObject.Find("Sub Camera");
+
+        MainCam.SetActive(false);
+
+        enabled = false;
+        yield return new WaitForSeconds(10);
+        enabled = true;
+
+    }
+    
+    /*
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (MainCam.activeSelf)
+            {
+                MainCam.SetActive(false);
+                SubCam.SetActive(true);
+            }
+            else
+            {
+                MainCam.SetActive(true);
+                SubCam.SetActive(false);
+            }
+        }
+    }
+    */
+
+    void Update()
+    {
+        MainCam.SetActive(true);
+        SubCam.SetActive(false);
+    }
+
+}
