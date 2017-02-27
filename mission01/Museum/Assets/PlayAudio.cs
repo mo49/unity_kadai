e54fa7b80dc29f6c14ab90b593d7e19ebfa6a79b
mo49 <k.mochizuki.0409@gmail.com>
2017-02-27 21:53:12 +0900

    build

---
+++ mission01/Museum/Assets/PlayAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+    public AudioClip BGM;
+    public float timer = 5.0f;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    IEnumerator Start () {
+
+        enabled = false;
+        yield return new WaitForSeconds(timer);
+        enabled = true;
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = BGM;
+
+        audioSource.Play();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
